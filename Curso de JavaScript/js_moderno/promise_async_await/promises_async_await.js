4d57fa8338a499d0e2f4bbd13a62214458817d50
Thalveex <thiago.alves0302@gmail.com>
2023-01-16 23:04:06 -0300

    testando promise,async e await

---
+++ Curso de JavaScript/js_moderno/promise_async_await/promises_async_await.js
@@ -0,0 +1,23 @@
+/*Promise*/
+function teste(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            const erro = false;
+        if(erro){
+            reject("erro...");
+        }else{
+            resolve("Promise resolvida!")
+        }
+        },5000)
+        
+    })
+}
+
+async function teste2(){
+    await teste().then(function(res){
+        alert(res);
+    });
+    alert("Testando");
+}
+
+teste2();
